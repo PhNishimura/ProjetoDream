e38ddc754d20de8266d45bf8aca9700037354c36
Pedro Nishimura <129108608+PhNishimura@users.noreply.github.com>
2024-10-30 17:04:59 -0300

    carrossel e botao
    Arruando carrossel e botao

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ import logo from'../img/logo.png'
 const Header = () => {
   return (
     <nav className="navbar">
+      <div className="logo">
+        <a href="#home">
+          <img src={logo} alt='logo' className='logo-image'/> 
+          Dream
+        </a>
+      </div>
       <div className="navbar-container">
-        <div className="logo">
-          <a href="#home">
-            <img src={logo} alt='logo' className='logo-image'/> 
-            Dream
-          </a>
-        </div>
         <ul className="nav-links">
           <li><a href="#home">Loja</a></li>
           <li><a href="#about">Sobre</a></li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ import Carousel from 'react-bootstrap/Carousel';
 import './main.css';
 import banner from "../img/banner.png";
 import test from "../img/jogo1.png";
-import tests from "../img/testes.jpg";
 
 const ExampleCarouselImage = ({ src, alt }) => (
   <img className="d-block w-100" src={src} alt={alt} style={{ height: '250px',  objectFit: 'cover' }}/>
@@ -15,7 +14,7 @@ const Main = () => {
     <main>
       <img src={banner} alt='banner' className='banner-image' />
       <div className='container'>
-        <h1>PROMOÇÕES</h1>
+        <h1 className='cB'>PROMOÇÕES</h1>
         <div className='box-promo'>
           <Carousel>
             <Carousel.Item>
@@ -28,10 +27,21 @@ const Main = () => {
               <ExampleCarouselImage src={test} alt="Terceiro Slide" />
             </Carousel.Item>
           </Carousel>
-        </div>
+          <div className='detais' >
+            <h1>THE LAST OF US II</h1>
+            <div data-tooltip="Price:R$59" class="button">
+              <div class="button-wrapper">
+                <div class="text">COMPRE JÁ!</div>
+                  <span class="icon">
+                    <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
-
-
     </main>
   );
 };
