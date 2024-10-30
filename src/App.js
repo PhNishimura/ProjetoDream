69840626e43035b3df93dbf56919c60d3a491e71
Pedro Nishimura <129108608+PhNishimura@users.noreply.github.com>
2024-10-30 12:30:03 -0300

    Main em produção
    Main em produção, ajustando carrossel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../src/components/Header';
 import Main from '../src/components/Main';
 import Footer from '../src/components/Foorter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const App = () => {
   return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 import './main.css';
-import banner from "../img/banner.png"
+import banner from "../img/banner.png";
+import test from "../img/jogo1.png";
+import tests from "../img/testes.jpg";
+
+const ExampleCarouselImage = ({ src, alt }) => (
+  <img className="d-block w-100" src={src} alt={alt} style={{ height: '250px',  objectFit: 'cover' }}/>
+);
+
 const Main = () => {
   return (
     <main>
-      <img src={banner} alt='banner' className='banner-image'/>
-
-      <div className='box-promo'>
+      <img src={banner} alt='banner' className='banner-image' />
+      <div className='container'>
         <h1>PROMOÇÕES</h1>
+        <div className='box-promo'>
+          <Carousel>
+            <Carousel.Item>
+              <ExampleCarouselImage src={test} alt="Primeiro Slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <ExampleCarouselImage src={test} alt="Segundo Slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <ExampleCarouselImage src={test} alt="Terceiro Slide" />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
-      <h1>PROMOÇÕES</h1>
+
+
     </main>
   );
 };
